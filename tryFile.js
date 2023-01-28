2f2341a7697862f45e17f11ab7f2d2db839ca215
Chris Udemans <chris.udemans@gmail.com>
2023-01-29 00:32:27 +0200

    chore: remove unnecessary files

--- tryFile.js
+++
@@ -1,8 +0,0 @@
-const { getMean, getMedian, getModem, getStandDev } = require("./index.js");
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// console.log(getMean(array));
-// console.log(getMedian(array));
-// console.log(getMode(array));
-console.log(getStandDev(array, (isSample = false)));

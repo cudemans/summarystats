4500959e58ea414949b204fb22b72dfc31dc6049
Chris Udemans <chris.udemans@gmail.com>
2023-02-05 23:00:04 +0200

    build: update harmonic mean

--- src/getHarmonicMean.js
+++ src/getHarmonicMean.js
@@ -1,11 +1,12 @@
 import { arrayError, lengthError } from "./errors.js";
+import sum from "./utils/sum.js";
 
 /**
- * In mathematics, the harmonic mean is one of several kinds of average, and in particular,
+ * The harmonic mean is one of several kinds of average, and in particular,
  * one of the Pythagorean means. It is sometimes appropriate for situations when the average rate is desired.
  * The harmonic mean can be expressed as the reciprocal of the arithmetic mean of the reciprocals of the given set of observations.
  * It can be calculated by dividing the number of observations by the sum of the reciprocals of all the values.
- * For example, the harmonic mean of 1, 4, 4 would be 3 / (1 / 1) + (1 / 4) + (1/ 4)
+ * For example, the harmonic mean of 1, 4, 4 would be 3 / (1 / 1) + (1 / 4) + (1/ 4).
  * -- [Wikipedia](https://en.wikipedia.org/wiki/Harmonic_mean)
  * @param array {Array} - Array of numbers
  * @returns {Number}  Harmonic mean of the array
@@ -16,5 +17,5 @@ export default function getHarmonicMean(array) {
   arrayError(array);
   lengthError(array, 1);
 
-  return array.length / array.map((d) => 1 / d).reduce((a, b) => a + b, 0);
+  return array.length / sum(array.map((d) => 1 / d));
 }

05859a72ded3975d6285743cc31139a7d00fbda1
Chris Udemans <chris.udemans@gmail.com>
2023-03-05 13:14:11 +0200

    build: allow multiple ways to import

--- src/index.js
+++ src/index.js
@@ -36,3 +36,52 @@ export { default as getKurtosis } from "./getKurtosis.js";
 
 // Utility functions
 export { default as matrixArrayIterator } from "./utils/matrixArrayIterator.js";
+
+// Export stats object
+import getMean from "./getMean.js";
+import getTrimmedMean from "./getTrimmedMean.js";
+import getHarmonicMean from "./getHarmonicMean.js";
+import getGeometricMean from "./getGeometricMean.js";
+import getWeightedMean from "./getWeightedMean.js";
+import getMedian from "./getMedian.js";
+import getMode from "./getMode.js";
+import getVariance from "./getVariance.js";
+import getStandDev from "./getStandDev.js";
+import getPercentile from "./getPercentile.js";
+import getQuartiles from "./getQuartiles.js";
+import getRange from "./getRange.js";
+import getIQR from "./getIQR.js";
+import getCoV from "./getCoV.js";
+import getMAD from "./getMAD.js";
+import getCorrCoeff from "./getCorrCoeff.js";
+import getZScore from "./getZScore.js";
+import getStandardError from "./getStandardError.js";
+import getKurtosis from "./getKurtosis.js";
+import getSkewness from "./getSkewness.js";
+import matrixArrayIterator from "./utils/matrixArrayIterator.js";
+
+const stats = {
+  getMean,
+  getTrimmedMean,
+  getHarmonicMean,
+  getGeometricMean,
+  getWeightedMean,
+  getMedian,
+  getMode,
+  getVariance,
+  getStandDev,
+  getPercentile,
+  getQuartiles,
+  getRange,
+  getIQR,
+  getCoV,
+  getMAD,
+  getCorrCoeff,
+  getZScore,
+  getStandardError,
+  getKurtosis,
+  getSkewness,
+  matrixArrayIterator,
+};
+
+export default stats;
